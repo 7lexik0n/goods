007faf97dbb30571a3b5adf1dab284eca863ea55
7lexik0n <7lexik0n@gmail.com>
2019-04-08 22:34:45 +0300

    v1.0

---
+++ inventoryChecker.js
@@ -0,0 +1,25 @@
+var table = document.createElement('table')
+var mainPage = document.querySelector('.cin');
+var firstGift = document.querySelector('.gift');
+mainPage.insertBefore(table, firstGift);
+let giftBlocks = document.querySelectorAll('.gift');
+var itemList = {};
+for (let i = 0; i < giftBlocks.length; i++) {
+	let name = giftBlocks[i].getAttribute('data-title');	
+	let number = giftBlocks[i].querySelector('.num').innerText;	
+	itemList[name] = number;
+}
+let orderedList = {};
+Object.keys(itemList).sort().forEach(function(key) {
+  orderedList[key] = itemList[key];
+});
+for (var key in orderedList) {
+	var tableStroke = document.createElement('tr');
+	table.appendChild(tableStroke);
+	var tableCell = document.createElement('td');
+	tableStroke.appendChild(tableCell);
+	tableCell.innerText = key;
+	var tableCell = document.createElement('td');
+	tableStroke.appendChild(tableCell);
+	tableCell.innerText = orderedList[key];
+} 
