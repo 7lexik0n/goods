77eeb1cfbfe9bcbaf1e2cf459e94ad32b6cc6b0f
7lexik0n <7lexik0n@gmail.com>
2019-04-08 23:00:54 +0300

    v1.0

---
+++ collectionsChecker.js
@@ -0,0 +1,49 @@
+let collectionBlocks = document.querySelectorAll('.block_6');
+let collections = [];
+for (let i = 0; i < collectionBlocks.length - 1; i++) {
+	var collection = {
+		title: collectionBlocks[i].querySelector('h3').innerText
+	}
+	var itemBlocks = collectionBlocks[i].querySelectorAll('.gift');
+	var count = 0;
+	for (let j = 0; j < itemBlocks.length; j++) {		
+		var name = itemBlocks[j].children[0].children[0].alt;
+		if (itemBlocks[j].children[0].tagName != 'A') {
+			var success = '+';
+			count++;
+		} else var success = '-';
+		collection[name] = success;
+	}
+	collection['count'] = count + '/' + itemBlocks.length;
+	collections.push(collection);
+}
+var table = document.createElement('table')
+var mainPage = document.querySelector('.main_aray');
+var firstBlock = document.querySelector('.block_6');
+mainPage.insertBefore(table, firstBlock);
+for (let i = 0; i < collections.length; i++) {
+	var tableStroke = document.createElement('tr');
+	table.appendChild(tableStroke);
+	var tableCell = document.createElement('td');
+	tableStroke.appendChild(tableCell);
+	tableCell.innerText = collections[i].title.slice(4) + ' (' + collections[i].count + ')';
+	tableStroke.style.fontWeight = 'bold';
+	tableStroke.style.fontSize = '18px';
+	tableCell.style.borderTop = '1px solid #333';
+	tableCell.style.borderBottom = '1px solid #333';
+	delete collections[i].title;
+	delete collections[i].count;
+	Object.keys(collections[i]).sort().forEach(function(key) {
+		var tableStroke = document.createElement('tr');
+		table.appendChild(tableStroke);
+		var tableCell = document.createElement('td');
+		tableStroke.appendChild(tableCell);
+		tableCell.innerText = key;
+		var tableCell = document.createElement('td');
+		tableStroke.appendChild(tableCell);
+		tableCell.innerText = collections[i][key];
+		if (collections[i][key] == '+') tableStroke.style.fontWeight = 'bold'
+		else tableStroke.style.color = '#7c1b1b';
+	});
+}
+table.style.backgroundColor = '#e8c77b';
